2f785e924e624b8954ff4fd9891e80ebdf8e2647
Ricio Dax Brathwaite <dax.dacosta@gmail.com>
2023-08-29 23:55:34 -0600

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include "std_lib_facilities.h"
+
+int main()
+{
+	vector <string> names{};
+	vector <string> scores{};
+
+	while (true) {
+		std::cout << "Enter a name with an age: ";
+	}
+	return 0;
+}
