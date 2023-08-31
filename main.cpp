a890bdfa1f724bd9e5fb61daaf53a588e245a2ac
Ricio Dax Brathwaite <dax.dacosta@gmail.com>
2023-08-31 15:48:19 -0600

    final

--- main.cpp
+++ main.cpp
@@ -4,30 +4,37 @@ int main()
 {
 	vector <string> names{};
 	vector <int> scores{};
-	string name{""};
-	int score{1};
-	int counter{1};
+	string name{ "" };
+	int score{ 1 };
+	int counter{ 1 };
 
 	while (true) {
-		
+
 		cout << counter << ". Enter a name with a score: ";
 		cin >> name >> score;
 		if (name == "NoName" && score == 0) {
 			break;
 		}
-		names.push_back(name);
-		scores.push_back(score);
-		counter++;
-	}
-	
+		//---------------------Checking for duplicate name --- Try Mode checker instead
+		
+		bool duplicate = false;
+		for (int i = 0; i < names.size(); i++) {
+			if (names[i] == name) {
+				cout << "You can't enter the same name twice. Please try again!\n";
+				duplicate = true;
+				break;
+			}
+		}
 
-	for (string i : names) {
-		cout << i << " ";
+		if (!duplicate) {
+			names.push_back(name);
+			scores.push_back(score);
+			counter++;
+		}
 	}
-	cout << '\n';
-	for (int i : scores) {
-		cout << i << " ";
+	//------------------------Printing the vectors
+	for (int i = 0; i < names.size(); i++) {
+		cout << names[i] << ": " << scores[i] << '\n';
 	}
-	cout << '\n';
 	return 0;
 }
